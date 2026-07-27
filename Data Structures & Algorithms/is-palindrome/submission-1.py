9abf55acbe3ae378acdb7613964f13cf19257ffc
Nida <nidarzx@gmail.com>
2026-07-27 00:36:33 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #two pointers 
+        # one on the left and right comparing if same
+        l, r  =  0, len(s) - 1
+
+        while l < r:
+
+            #skip white space
+            while l < r and not s[l].isalnum(): 
+                l += 1 
+
+            while l < r and not s[r].isalnum():
+                r -= 1 
+
+            #if any not equal 
+            # convert to lowercase for case sensitivity
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1 #move the pointer to the right
+            r -= 1 #move the pointer to the left
+
+        return True 
+       # O(n) since there are n items we are going through 
+       # and O(1) space, nothing new is being created
